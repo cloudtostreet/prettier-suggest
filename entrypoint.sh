2f21c8853b66356bd8c860772d58cc04404012f4
Cole Erickson <cole@cloudtostreet.info>
2020-08-03 13:38:40 -0700

    Modify dependencies

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,17 @@
 ACCESS_TOKEN=$1
 SOURCE_PATH=$2
 
+# Enter the repo
 cd $(dirname $SOURCE_PATH)
-prettier $SOURCE_PATH --write
+
+# Format the files within SOURCE_PATH that have changed in the pull request
+prettier $(join <(find $SOURCE_PATH -type f | sort) <(git diff --name-only origin/master | sort)) --write
+
+# Export the changes as a patch file
 git diff > $HOME/prettier.patch
+
+# Revert the changes
 git checkout -- .
+
+# Apply the changes as comments on the PR
 python3 /apply_patches_as_suggestion.py --access-token $ACCESS_TOKEN $HOME/prettier.patch
