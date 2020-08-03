042a17bbbe5478b643bbf243f4b2b0110116ea51
Cole Erickson <cole@cloudtostreet.info>
2020-08-03 13:17:59 -0700

    Modify dependencies

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ SOURCE_PATH=$2
 
 cd $(dirname $SOURCE_PATH)
 prettier $SOURCE_PATH --write
-git diff > prettier.patch
+git diff > $HOME/prettier.patch
 python3 /apply_patches_as_suggestion.py --access-token $ACCESS_TOKEN $HOME/prettier.patch
